ad6654a3b8b3049ae07ce7970b129b3c0a466654
Théo Hervieu <theo.hervieu@devinci.fr>
2016-09-13 17:47:03 +0200

    Ajout des balises 'name' au formulaire de Login. #1

--- view/login.php
+++ view/login.php
@@ -21,10 +21,10 @@
 						</span>
 					</div>
 					<div class="form-submit">
-						<input type="submit" value="Connecter">	
+						<input type="submit" value="Connecter" name="button_connect">	
 					</div>
 					<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
 				</form>
 			</div>
 		</div>
-	</section>
+	</section> 
